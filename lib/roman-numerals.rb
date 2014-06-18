20732ee5a7ef6e84f9bf17543e73f28678df7fde
Doug Emery <emeryr@upenn.edu>
2014-06-18 11:57:26 -0400

    For medieval roman 4's use IIII: RomanNumerals.to_medieval(decimal)

--- lib/roman-numerals.rb
+++ lib/roman-numerals.rb
@@ -15,15 +15,14 @@ module RomanNumerals
     1000 => 'M'
   }
 
+  @medieval_digits = @base_digits.reject { |k,v| [4,9].include?(k) }
+
+  def self.to_medieval value
+    build_roman @medieval_digits, value
+  end
+
   def self.to_roman value
-    result = ''
-    @base_digits.keys.reverse.each do |decimal|
-      while value >= decimal
-        value -= decimal
-        result += @base_digits[decimal]
-      end
-    end
-    result
+    build_roman @base_digits, value
   end
 
   def self.to_decimal value
@@ -34,6 +33,19 @@ module RomanNumerals
         value = value.slice(roman.length, value.length)
         result += @base_digits.key roman
       end
+    end
+    result
+  end
+
+  private
+  def self.build_roman digits, value
+    result = ''
+    digits.keys.reverse.each do |decimal|
+      while value >= decimal
+        value -= decimal
+        result += digits[decimal]
+      end
+      result
     end
     result
   end
--- spec/roman-numerals_spec.rb
+++ spec/roman-numerals_spec.rb
@@ -19,6 +19,36 @@ describe RomanNumerals do
     1000 => 'M'
   }
 
+    medieval_digits = {
+    1    => 'I',
+    5    => 'V',
+    10   => 'X',
+    40   => 'XL',
+    50   => 'L',
+    90   => 'XC',
+    100  => 'C',
+    400  => 'CD',
+    500  => 'D',
+    900  => 'CM',
+    1000 => 'M'
+  }
+
+  describe ".to_medieval" do
+    medieval_digits.each do |decimal, roman|
+      it "converts the decimal value #{decimal} to the medieval roman value #{roman}" do
+        RomanNumerals.to_medieval(decimal).should == roman
+      end
+    end
+    it "converts larger decimals" do
+      RomanNumerals.to_medieval(23234).should == 'MMMMMMMMMMMMMMMMMMMMMMMCCXXXIIII'
+      RomanNumerals.to_medieval(42).should == 'XLII'
+      RomanNumerals.to_medieval(4).should == 'IIII'
+      RomanNumerals.to_medieval(14).should == 'XIIII'
+      RomanNumerals.to_medieval(9).should == 'VIIII'
+      RomanNumerals.to_medieval(19).should == 'XVIIII'
+    end
+  end
+
   describe ".to_roman" do
     base_digits.each do |decimal, roman|
       it "converts the decimal value #{decimal} to the roman value #{roman}" do
